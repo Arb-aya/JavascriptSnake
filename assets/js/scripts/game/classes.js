d849f6fa0aedee9d752d4d5bae806644ac53c893
Christopher Hayden <Chayden50@gmail.com>
2019-10-19 22:32:00 +0100

    Create Snake class

--- assets/js/scripts/game/classes.js
+++ assets/js/scripts/game/classes.js
@@ -1,6 +1,6 @@
- const loaded = true;
+ const classes_loaded = true;
  
-export default loaded;
+export default classes_loaded;
 
 /*
  * Class to wrap the Canvas.
@@ -18,6 +18,13 @@ export class GameStage {
 
         //context object from canvas
         this._context = this._canvas.getContext("2d");
+    }
+
+    //Methods
+
+    clear()
+    {
+        this._context.clearRect(0,0, this._canvas.width, this._canvas.height);
     }
 
     //Getters and setters
@@ -42,6 +49,47 @@ export class GameStage {
     set width(width) {}
     set height(height){}
 
+}
+
+
+export class Snake{
+    constructor(x, y, context, colour = "red"){
+        this._x = x;
+        this._y = y;
+
+        this._height = 10;
+        this._width = 10;
+
+        this._colour = colour;
+
+        this._context = context;
+    }
+
+    draw(){
+        this._context.fillStyle = this._colour;
+        this._context.fillRect(this._x, this._y, this._width, this._height);
+    }
+
+    // Getters and Setters
+    get x()
+    {
+        return this._x;
+    }
+
+    set x(x)
+    {
+        this._x = x;
+    }
+
+    get y()
+    {
+        return this._y;
+    }
+
+    set y(y)
+    {
+        this._y = y;
+    }
 }
 
 
