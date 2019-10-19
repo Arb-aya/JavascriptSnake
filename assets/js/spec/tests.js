7b10b338ad4434d0eff6b1f251ff0a8220cc691f
Christopher Hayden <Chayden50@gmail.com>
2019-10-19 21:36:14 +0100

    Structure tests to work properly with Jasmine

--- assets/js/spec/tests.js
+++ assets/js/spec/tests.js
@@ -1,5 +1,23 @@
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-      expect(true).toBe(true);
+import loaded, { GameStage } from "../scripts/game/classes.js";
+
+describe("GameStage class", function () {
+    const INITIAL_WIDTH = 300, INITIAL_HEIGHT = 300;
+    const game = new GameStage(300, 300);
+    it("Should have created a canvas with id of gameCanvas", function () {
+
+        expect(document.getElementById("gameCanvas")).not.toEqual(null);
     });
-  });
+
+    describe("Getters and Setters", function () {
+        it("Should return width", function () {
+            expect(game.width).toBe(INITIAL_WIDTH);
+        });
+
+        it("Should set width", function(){
+            let newWidth = 200;
+            game.width = newWidth;
+            expect(game.width).toBe(newWidth);
+        });
+    });
+
+});
