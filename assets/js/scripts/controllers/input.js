301090b8d967885c084ff8dfdc5ab59e3466fcbc
Christopher Hayden <Chayden50@gmail.com>
2019-10-20 21:44:49 +0100

    Document class properly

--- assets/js/scripts/controllers/input.js
+++ assets/js/scripts/controllers/input.js
@@ -2,8 +2,18 @@ const input_loaded = true;
 
 export default input_loaded;
 
+/**
+ * Class used to map keycodes and return a direction string
+ * based on a keycode
+ */
 export class KeyMappings {
-    //Default mapped to the arrow keys
+    /**
+     * 
+     * @param {number} upKey    Keycode value for up
+     * @param {number} downKey  Keycode value for down
+     * @param {number} leftKey  Keycode value for left 
+     * @param {number} rightKey Keycode value for right
+     */
     constructor(upKey = 38, downKey = 40, leftKey = 37, rightKey = 39) {
         this._up = upKey;
         this._down = downKey;
@@ -11,9 +21,10 @@ export class KeyMappings {
         this._right = rightKey;
     }
 
-    /* If the given keycode matches any of the mapped
-     * up, down, left or right keys it returns a string
-     * with the given direction. Otherwise returns false
+    /**
+     * Turns keycode to direction it is mapped to.
+     * @param {number} key Keycode we want to turn into a direction string
+     * @return {string} "UP,DOWN,LEFT,RIGHT" if matched, false if no match.
      */
     getDirection(key) {
         switch (key) {
@@ -34,35 +45,58 @@ export class KeyMappings {
         }
     }
 
-    //Setters and getters
+    /**
+     * @param {number} key keycode to map to up
+     */
     set up(key) {
         this._up = key;
     }
 
+    /**
+     * Get keycode mapped to up
+     */
     get up() {
         return this._up;
     }
 
+    /**
+     * @param {number} key keycode to map to down
+     */
     set down(key) {
         this._down = key;
     }
 
+    /**
+     * Get keycode mapped to down
+     */
     get down() {
         return this._down;
     }
 
+    /**
+     * @param {number} key keycode to map to left
+     */
     set left(key) {
         this._left = key;
     }
 
+    /**
+     * Get keycode mapped to left
+     */
     get left() {
         return this._left;
     }
 
+    /**
+     * @param {number} key keycode to map to right
+     */
     set right(key) {
         this._right = key;
     }
 
+    /**
+     * Get keycode mapped to right
+     */
     get right() {
         return this._right;
     }
