e902a31128c7ca70f3c13d691d0d2a73dcab57ee
Christopher Hayden <Chayden50@gmail.com>
2019-10-24 20:38:03 +0100

    Comment classes

--- assets/js/scripts/game/classes.js
+++ assets/js/scripts/game/classes.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const classes_loaded = true;
 
@@ -64,11 +64,20 @@ export class GameStage {
 }
 
 /**
- * Abstract SnakePart that contains common functionality for both
- * the SnakeBody and SnakeHead.
+ *  Base class for each part of the snake. Contains basic functionality to move and draw it.
  */
 class SnakePart {
 
+    /**
+     *  Constructs a new colour snakepart at x,y via context at a size of size.
+     * @param    {number}                          x        x location for this snake part
+     * @param    {number}                          y        y location for this snake part
+     * @param    {CanvasRenderingContext2D }       context  context object from a html 5 canvas     
+     * @param    {string}                          colour   colour of this snake part
+     * @param    {number}                          size     defines the height and width of this unit
+     * @property {number}                          prevX    When the x location changes, the old location is stored in prevX
+     * @property {number}                          prevY    When the y location changes, the old location is stored in prevY
+     */
     constructor(x, y, context, colour, size) {
         this._x = x;
         this._y = y;
@@ -84,61 +93,108 @@ class SnakePart {
 
     }
 
+    /**
+     * Draws the snakepart on the html5 canvas, via the context object.
+     */
     draw() {
         this._context.fillStyle = this._colour;
         this._context.fillRect(this._x, this._y, this._size, this._size);
     }
 
+    /**
+     * @param {number} x the new x position to change to
+     * Stores the old x value in prevX
+     */
     set x(x) {
         this._prevX = this._x;
         this._x = x;
     }
 
+    /**
+     * @param {number} y the new y position to change to
+     * Stores the old y value in prevY
+     */
     set y(y) {
         this._prevY = this._y;
         this._y = y;
     }
 
+    /**
+     * Get snakepart's x location
+     */
     get x() {
         return this._x;
     }
 
+    /**
+     * Get snakepart's y location
+     */
     get y() {
         return this._y;
     }
 
+    /**
+     * Get the old x location of this snakepart
+     */
     get prevX() {
         return this._prevX;
     }
 
+    /**
+     * Get the old y location of this snakepart
+     */
     get prevY() {
         return this._prevY;
     }
 
+    /**
+     * Get the colour of this snakepart
+     */
     get colour() {
         return this._colour;
     }
 
+    /**
+     * @param {string} colour   The colour this snakepart should be
+     */
     set colour(colour) {
         this._colour = colour;
     }
 
+    /**
+     * Get the size of this snakepart
+     */
     get size() {
         return this._size;
     }
 
+    /**
+     * @param {number} size Size the snakepart should be. Used for height and width.
+     */
     set size(size) {
         this._size = size;
     }
 }
 
+/**
+ * Extends snakepart class with added functionality of moving and direction.
+ * @see SnakePart
+ */
 class SnakeHead extends SnakePart {
 
+    /**
+     * @see SnakePart
+     * @param {string} initialDirection Direction the snake should start moving in
+     */
     constructor(x, y, context, initialDirection, colour, size) {
         super(x, y, context, colour, size);
         this._direction = initialDirection;
     }
 
+    /**
+     * Changes the x or y location of the snake based on snake's direction
+     * @param {direction} [direction = this.direction] Direction to move in 
+     */
     move(direction = this._direction) {
         switch (direction) {
             case "UP":
@@ -181,7 +237,16 @@ class SnakeHead extends SnakePart {
     }
 }
 
+/**
+ * Snake class manages the snake body and moving each part of the body.
+ */
 export class Snake {
+    /**
+     * @see SnakePart
+     * @see SnakeHead
+     * @param {number}                  startingBodyLength How many units the body should be composed of at the start of the game
+     * @property {SnakePart/SnakeHead}  body               Array that contains SnakeHead and SnakePart. SnakeHead always the first element.
+     */
     constructor(x, y, context, startingBodyLength = 5, intitalDirection = "UP", unitSize = 10, colour = "red") {
         this._body = [];
         const head = new SnakeHead(x, y, context, intitalDirection, colour, unitSize);
@@ -193,12 +258,21 @@ export class Snake {
     }
 
 
+    /**
+     * Call the draw method on each snake part.
+     */
     draw() {
         this._body.forEach(function (part) {
             part.draw();
         });
     }
 
+    /**
+     * Calls the move method of the SnakeHead. For each body part
+     * sets the x and y coordinate to the prevX and prevY of the element
+     * before it in the array.
+     * @param {string} direction Direction to move the snake in.
+     */
     move(direction) {
 
         this._body[0].move(direction);
@@ -210,10 +284,9 @@ export class Snake {
 
     }
 
-    changeDirection(direction) {
-        this._body[0].direction = direction;
-    }
-
+    /**
+     * Get the direction of the head of the snake.
+     */
     getDirection() {
         return this._body[0].direction;
     }
